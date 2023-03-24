114f062cf52df48871a0589961d83805ed574ea9
Татьяна <lady.dzhiya@mail.ru>
2023-03-24 20:54:32 +0500

    последний коммит

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = { "hello", "2", "world", ":-0"};
+int number = 4;
+
+array = array.Where(e => e.Length < number).ToArray();
+
+for (int i = 0; i < array.Length; i++){
+    Console.WriteLine(array[i]);
+}
